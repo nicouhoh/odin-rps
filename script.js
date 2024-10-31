1a1012efb721b12f38ac8e4b458517a7ac149989
Nico Ramsey <nicouhoh@gmail.com>
2024-10-30 21:42:57 -0400

    Add function computerChoice()

---
+++ script.js
@@ -0,0 +1,8 @@
+//get computer choice
+function computerChoice() {
+  let cpuNumber = Math.random();
+  return cpuNumber < 1 / 3 ? "rock" : cpuNumber < 2 / 3 ? "paper" : "scissors";
+}
+// get human choice
+//
+// declare t
